aef43c66f4603c4fd0b4837f6029a179fcac1086
Yesid <trigoperfec3@hotmail.com>
2020-08-05 20:26:51 -0500

    transcripcion de la primera parte del codigo de java a kotlin en ActivityVideollamada

---
+++ app/src/main/java/com/mitiempo/videollamada/vistaVideollamada/intento_3/ActividadVideollamada.kt
@@ -0,0 +1,201 @@
+package com.mitiempo.videollamada.vistaVideollamada.intento_3
+
+import android.Manifest
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.mitiempo.videollamada.R
+import com.mitiempo.videollamada.databinding.ActivitySamplePeerConnectionBinding
+import io.socket.client.IO
+import io.socket.client.Socket
+import io.socket.client.Socket.EVENT_CONNECT
+import org.json.JSONObject
+import org.webrtc.*
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
+
+
+class ActividadVideollamada : AppCompatActivity() {
+
+    companion object{
+
+        private const val TAG = "ActividadVideollamada"
+        private const val RC_CALL = 111
+        private const val VIDEO_TRACK_ID = "ARDAMSv0"
+        private const val VIDEO_RESOLUTION_WIDTH  = 1280
+        private const val VIDEO_RESOLUTION_HEIGTH = 720
+        private const val FPS = 30
+
+    }
+
+
+
+
+    private var isStarted = false
+
+    private var audioConstraints : MediaConstraints ?= null
+    private var AudioSource : AudioSource ?= null
+    private var localAudioTrack : AudioTrack ?= null
+
+    private var binding : ActivitySamplePeerConnectionBinding ?= null
+    private var peerConnection : PeerConnection ?= null
+    private var rootEglBase : EglBase ?= null
+    private var factory  : PeerConnectionFactory ?= null
+    private var videoTrackFromCamera : VideoTrack ?= null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sample_peer_connection)
+        start()
+    }
+
+    @AfterPermissionGranted(RC_CALL)
+    private fun start(){
+        if(!tengoLosPermisos()){
+            EasyPermissions.requestPermissions(this,getString(R.string.traer_permisos), RC_CALL,Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO)
+            return
+        }
+
+        connectToSignallingServer()
+
+    }
+
+
+    private fun tengoLosPermisos() : Boolean{
+        return EasyPermissions.hasPermissions(this,Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults)
+    }
+
+    private var socket : Socket ?= null
+    private val hostingVideollamada = "http://192.168.0.3:3000/"
+    private val crearOUnirHabitacion = "create or join"
+    private val habitacion = "foo"
+    private val ipaddr = "ipaddr"
+    private val created = "created"
+    private var isInitiator = false
+    private val full = "full"
+    private val join = "join"
+    private var isChannelReady = false
+    private val joined = "joined"
+    private val log = "log"
+    private val message = "message"
+    private val type = "type"
+    private val offer = "offer"
+    private val answer = "answer"
+    private val sdp = "sdp"
+    private val candidate = "candidate"
+    private val id = "id"
+    private val label  = "label"
+    private val disconnect = "disconnect"
+    private fun connectToSignallingServer(){
+
+        try {
+            socket = IO.socket(hostingVideollamada)
+
+            socket?.on(EVENT_CONNECT){
+                    args ->
+                imprimirEnConsolaConnectSignallingServer(EVENT_CONNECT)
+                socket?.emit(crearOUnirHabitacion,habitacion)
+            }?.on(ipaddr){imprimirEnConsolaConnectSignallingServer(ipaddr)}
+                ?.on(created){
+                    imprimirEnConsolaConnectSignallingServer(created)
+                    isInitiator = true
+                }?.on(full){
+                    imprimirEnConsolaConnectSignallingServer(full)
+                }?.on(join){
+                    imprimirEnConsolaConnectSignallingServer(join)
+                        .imprimirEnConsolaConnectSignallingServer("Another peer made a request to join room ")
+                        .imprimirEnConsolaConnectSignallingServer("this peer is initiator of room")
+                    isChannelReady = true
+                }?.on(joined){
+                    imprimirEnConsolaConnectSignallingServer(joined)
+                    isChannelReady = true
+                }?.on(log){
+                        args-> args.forEach {
+                    imprimirEnConsolaConnectSignallingServer(" $it ")
+                }
+                }?.on(message){
+                    imprimirEnConsolaConnectSignallingServer(message)
+                }?.on(message){
+                        args ->
+                    try {
+
+                        if(args[0] is String){
+                            if(args[0].equals("got user media")){
+                                maybeStart()
+                            }
+
+                        }else{
+
+                            val mensaje = args[0] as JSONObject
+                            imprimirEnConsolaConnectSignallingServer(message)
+
+                            if(mensaje.getString(type).equals(offer)){
+
+                                imprimirEnConsolaConnectSignallingServer(" $isInitiator $isStarted")
+                                if(!isInitiator && !isStarted){
+                                    maybeStart()
+                                }
+
+                                peerConnection?.setRemoteDescription(SimpleSdpObserver(),SessionDescription(SessionDescription.Type.OFFER,mensaje.getString(sdp)))
+                                doAnswer()
+
+                            }else if(mensaje.getString(type).equals(answer)){
+
+                                peerConnection?.setRemoteDescription(SimpleSdpObserver(),SessionDescription(SessionDescription.Type.ANSWER,mensaje.getString(sdp)))
+
+                            }else if(mensaje.getString(type).equals(candidate) && isStarted){
+
+                                imprimirEnConsolaConnectSignallingServer("receiving candidates")
+                                val candidate = IceCandidate(mensaje.getString(id),mensaje.getInt(label),mensaje.getString(candidate))
+                                peerConnection?.addIceCandidate(candidate)
+
+                            }
+
+                        }
+                    }catch (e :Exception){
+                        e.printStackTrace()
+                    }
+                }?.on(EVENT_CONNECT){
+                    imprimirEnConsolaConnectSignallingServer(disconnect)
+                }
+            socket?.connect()
+
+        }catch (e : Exception){
+            e.printStackTrace()
+        }
+    }
+
+    private fun imprimirEnConsolaConnectSignallingServer(mensaje : String) : ActividadVideollamada{
+        Log.d(TAG,"connectToSignallingServer : $mensaje")
+        return this
+    }
+
+    private fun maybeStart(){
+        Log.d(TAG, "maybeStart $isStarted $isChannelReady")
+        if(!isStarted && isChannelReady){
+            isStarted = true
+            if(isInitiator){
+                doCall()
+            }
+        }
+    }
+
+    private fun doCall(){
+
+    }
+
+    private fun doAnswer(){
+
+    }
+
+}
